a7737e97d9bebb5e5e2e8fabd0dd089247bbaf32
dhirendra <shindedhirendra780@gmail.com>
2024-09-10 18:51:17 +0530

    render Links to backend

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="http://localhost:3000/api/v1/user"
-export const JOB_END_POINT='http://localhost:3000/api/v1/job'
-export const APPLICATION_END_POINT='http://localhost:3000/api/v1/application'
-export const COMPANY_END_POINT='http://localhost:3000/api/v1/company'
+export const USER_API_END_POINT="https://job-portal-mern-yjir.onrender.com/api/v1/user"
+export const JOB_END_POINT='https://job-portal-mern-yjir.onrender.com/api/v1/job'
+export const APPLICATION_END_POINT='https://job-portal-mern-yjir.onrender.com/api/v1/application'
+export const COMPANY_END_POINT='https://job-portal-mern-yjir.onrender.com/api/v1/company'
