6a9dba67cc746c84e35ce4bf794ee6f6a954b9ce
dhirendra <shindedhirendra780@gmail.com>
2024-09-10 19:03:16 +0530

    cors url change

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 const corsOption={
-  origin:'*',
+  origin:'https://job-portal-mern-omega.vercel.app',
   credentials:true,
   methods:['GET','POST','PUT','DELETE']
 }
